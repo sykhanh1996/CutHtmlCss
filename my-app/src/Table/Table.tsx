6844c3482dfe8a7a5fe6624d9ec1ed0fab04768f
Khanh Sy <sykhanh1996@gmail.com>
2023-12-24 00:58:52 +0700

    solution render

--- my-app/src/Table/Table.tsx
+++ my-app/src/Table/Table.tsx
@@ -1,91 +1,237 @@
-import { Space, Tag } from "antd";
+import {
+  Button,
+  Checkbox,
+  DatePicker,
+  Form,
+  FormInstance,
+  Input,
+  Select,
+  Space,
+  Tag,
+} from "antd";
 import Table, { ColumnsType } from "antd/es/table";
-import React from "react";
+import React, { useState } from "react";
+import { isNil } from "lodash";
 
 interface DataType {
   key: string;
   name: string;
-  age: number;
-  address: string;
+  type: { label: string; value: number }[];
+  address: { label: string; value: number }[];
   tags: string[];
+  custom: string;
+  typeSelected?: number;
 }
 
-const columns: ColumnsType<DataType> = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: "Age",
-    dataIndex: "age",
-    key: "age",
-  },
-  {
-    title: "Address",
-    dataIndex: "address",
-    key: "address",
-  },
-  {
-    title: "Tags",
-    key: "tags",
-    dataIndex: "tags",
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? "geekblue" : "green";
-          if (tag === "loser") {
-            color = "volcano";
+const columns = (
+  handleChangeSelect: () => (value: number) => void
+): ColumnsType<DataType> => {
+  return [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+      // onCell: (record, rowIndex) => {
+      //   return {
+      //     onClick: (ev) => {
+      //       console.log(record, rowIndex);
+      //     },
+      //   };
+      // },
+      render: (text, record, index) => (
+        <>
+          <Form.Item
+            name={["data", "row" + index, "type"]}
+            initialValue={record?.typeSelected}
+          >
+            <Select
+              style={{ width: 120 }}
+              onChange={handleChangeSelect()}
+              options={record.type}
+            />
+          </Form.Item>
+        </>
+      ),
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+      render: (text, record, index) => (
+        <>
+          <Form.Item
+            name={["data", "row" + index, "address"]}
+            initialValue={record?.typeSelected}
+          >
+            <Select style={{ width: 120 }} options={record.address} />
+          </Form.Item>
+        </>
+      ),
+    },
+    {
+      title: "Tags",
+      key: "tags",
+      dataIndex: "tags",
+      render: (_, record) => (
+        <>
+          {record.tags.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+              color = "volcano";
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) =>
+        record.type.length > 0 ? <>test</> : <>no record</>,
+    },
+    {
+      title: "Custom",
+      key: "custom",
+      render: (_, record) => {
+        if (!isNil(record?.typeSelected)) {
+          switch (record.typeSelected) {
+            case 1:
+              return (
+                <Form.Item name="custom">
+                  <Checkbox defaultChecked={false} />
+                </Form.Item>
+              );
+            default:
+              return (
+                <Form.Item name="custom">
+                  <DatePicker />
+                </Form.Item>
+              );
           }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
+        }
+        return (
+          <Form.Item name="custom">
+            <Input></Input>
+          </Form.Item>
+        );
+      },
+    },
+  ];
+};
 
 const data: DataType[] = [
   {
     key: "1",
     name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
+    type: [
+      {
+        label: "select 1",
+        value: 1,
+      },
+      {
+        label: "select 2",
+        value: 2,
+      },
+    ],
+    address: [
+      {
+        label: "address 1",
+        value: 1,
+      },
+      {
+        label: "address 2",
+        value: 2,
+      },
+    ],
     tags: ["nice", "developer"],
+    custom: "",
   },
   {
     key: "2",
     name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
+    type: [
+      {
+        label: "select 1",
+        value: 1,
+      },
+      {
+        label: "select 2",
+        value: 2,
+      },
+    ],
+    address: [
+      {
+        label: "address 1",
+        value: 1,
+      },
+      {
+        label: "address 2",
+        value: 2,
+      },
+    ],
     tags: ["loser"],
+    custom: "",
   },
   {
     key: "3",
     name: "Joe Black",
-    age: 32,
-    address: "Sydney No. 1 Lake Park",
+    type: [],
+    address: [
+      {
+        label: "address 1",
+        value: 1,
+      },
+      {
+        label: "address 2",
+        value: 2,
+      },
+    ],
     tags: ["cool", "teacher"],
+    custom: "",
   },
 ];
+
 export default function CustomTable() {
+  const [testData, setTestData] = useState<DataType[]>(data);
+  const handleChangeSelect = () => (value: number) => {
+    // console.log(value);
+    if (value == 1) {
+      setTestData((prev) => {
+        return prev;
+      });
+    }
+  };
+
+  const onFinish = (values: any) => {
+    // console.log(values);
+  };
   return (
-    <div>
-      <Table columns={columns} dataSource={data} />
-    </div>
+    <>
+      <div className="container mx-auto mt-5">
+        <Form onFinish={onFinish}>
+          <Table
+            columns={columns(handleChangeSelect)}
+            dataSource={testData}
+            pagination={false}
+          />
+          <div className="w-full flex">
+            <Button type="default" htmlType="submit" className="mt-2 m-auto">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </>
   );
 }
