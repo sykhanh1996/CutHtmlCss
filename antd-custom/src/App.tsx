b721b40c8a9677b85edd5b08c49e1da5069b5543
KayNguyen <kaynguyen@developertools.com>
2025-01-03 10:45:26 +0700

    test 3

--- antd-custom/src/App.tsx
+++ antd-custom/src/App.tsx
@@ -1,16 +1,14 @@
-import FloatingLabel from "./components/FloatingLabel";
+import FloatingLabel from './components/FloatingLabel'
 
 function App() {
   return (
     <>
-      <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold underline text-blue-500">
-          Hello world!
-        </h1>
+      <div className='container mx-auto px-4'>
+        <h1 className='text-3xl font-bold underline text-blue-500'>Hello world! test3</h1>
         <FloatingLabel />
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
