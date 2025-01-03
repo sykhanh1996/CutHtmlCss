5ec9028ff85f11f77e884b306c04701730027e01
KayNguyen <kaynguyen@developertools.com>
2025-01-03 10:44:48 +0700

    test EDIT REBASE
    test 2
    
    test Edit rebase 2
    
    test 2

--- antd-custom/src/App.tsx
+++ antd-custom/src/App.tsx
@@ -1,16 +1,16 @@
-import FloatingLabel from "./components/FloatingLabel";
+import FloatingLabel from './components/FloatingLabel'
 
 function App() {
   return (
     <>
-      <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold underline text-blue-500">
-          Hello world!
-        </h1>
+      <div className='container mx-auto px-4'>
+        <h1 className='text-3xl font-bold underline text-blue-500'>Hello world!</h1>
         <FloatingLabel />
       </div>
     </>
-  );
+  )
 }
+export const test = '1'
+export const test2 = '2'
 
-export default App;
+export default App
